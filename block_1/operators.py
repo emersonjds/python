e3bf9532072b5250e53d640106a7818d0fc78c16
Emerson Silva <emerson_jdss@hotmail.com>
2025-02-18 23:18:27 -0300

    understanding operators

---
+++ block_1/operators.py
@@ -0,0 +1,22 @@
+a=5
+b=10
+
+# Common operators in python are:
+print(a+b)
+
+print(a-b)
+
+print(a*b)
+
+print(a/b)
+
+# numero elevado a potencia
+print(a**b)
+# In this case, the output will be:
+# 15
+
+#radiciação da divisão
+print(a//b)
+# In this case, the output will be:
+# -5
+
