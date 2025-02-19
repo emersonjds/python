7099c3d736277b17dc208da085b55d782f271d67
Emerson Silva <emerson_jdss@hotmail.com>
2025-02-18 22:04:36 -0300

    string functions

--- block_1/caracters.py
+++ block_1/caracters.py
@@ -7,3 +7,22 @@ print("My name is" + nome)
 msg="Hello, " * 3
 print(msg)
 # output = "Hello, Hello, Hello, "
+
+# String functions
+print(nome.upper())
+# output = "ROBERT"
+
+print(nome.lower())
+# output = "robert"
+
+print(nome.capitalize())
+# output = "Robert"
+
+print(nome.strip())
+# output = "Robert"
+
+print(nome.replace("R", "P"))
+# output = "Pobert"
+
+print(nome.split("b"))
+# output = ['Ro', 'ert']
