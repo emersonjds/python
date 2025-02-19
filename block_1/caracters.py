2a8f49278aa99dc86ba49d82c010d18aabf98549
Emerson Silva <emerson_jdss@hotmail.com>
2025-02-18 22:03:04 -0300

    multiply string

--- block_1/caracters.py
+++ block_1/caracters.py
@@ -2,3 +2,8 @@ nome="Robert"
 
 # concat like js
 print("My name is" + nome)
+
+# in Python it's possible multiply strings like this:
+msg="Hello, " * 3
+print(msg)
+# output = "Hello, Hello, Hello, "
