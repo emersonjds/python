a9980284fc3e5a16090b10a096c7b6645cca0615
Emerson Silva <emerson_jdss@hotmail.com>
2025-02-18 22:18:14 -0300

    conversion of data

--- block_1/strings.py
+++ block_1/strings.py
@@ -2,3 +2,9 @@ name="Emerson"
 secondName="Silva"
 
 print("Hello, my name is {} and my second name is {}".format(name, secondName))
+
+# conversion of data
+age = int(input("Enter your age: "))
+print("You are {} years old".format(age))
+
+# I need to convert the age variable to an integer because the input function returns a string. If I don't convert it, I will get an error when I try to use the variable in the format method.
