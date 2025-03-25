088e7a39ce0a50aa8e26235342f3bcfc0269de11
Emerson Silva <emerson_jdss@hotmail.com>
2025-03-24 22:47:52 -0300

    add block_3

---
+++ block_3/notes.py
@@ -0,0 +1,3 @@
+nota = float(input("Digite a média das notas que tirou nas provas semanais: "))
+nota2 = float((5 - nota * 0.4)/ 0.6)
+print("Você precisa tirar ", nota2, "para passar de bimestre.")
